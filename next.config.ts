98303e0bfa58d118e9bfc145c89c7ebe02cb88e1
Comnarin <Nsirinapuk@gmail.com>
2026-05-04 19:43:43 +0700

    init cicd

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
@@ -11,7 +12,7 @@ const nextConfig: NextConfig = {
   },
   allowedDevOrigins: ["*.trycloudflare.com"],
   async rewrites() {
-    const backendUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "") || "http://localhost:8080";
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "");
     return [
       {
         source: "/api/v1/:path*",
